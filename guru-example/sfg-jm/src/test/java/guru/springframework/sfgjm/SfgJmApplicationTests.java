67b72c90f8c664bda8a0d1aad3805d0d1a98b991
Fedor Sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-11-07 22:31:22 +0200

    git new project

---
+++ guru-example/sfg-jm/src/test/java/guru/springframework/sfgjm/SfgJmApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
