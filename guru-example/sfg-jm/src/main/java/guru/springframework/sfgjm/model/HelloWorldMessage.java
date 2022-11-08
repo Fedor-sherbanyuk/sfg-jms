cb7cd416bb5f5f4c546f724ec8ea9bfd4fcdc9aa
Fedor Sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-11-08 10:08:56 +0200

    class TaskConfig

---
+++ guru-example/sfg-jm/src/main/java/guru/springframework/sfgjm/model/HelloWorldMessage.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgjm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+    static final long serializableID=-42777777777777L;
+    private UUID uuid;
+    private String message;
+
+}
